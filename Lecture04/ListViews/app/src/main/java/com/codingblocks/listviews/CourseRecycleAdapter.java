e68c42d78f001c55b2133c3291f40875f5534599
Arnav Gupta <arnav@codingblocks.com>
2017-03-12 11:38:59 +0530

    view types
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture04/ListViews/app/src/main/java/com/codingblocks/listviews/CourseRecycleAdapter.java
+++ Lecture04/ListViews/app/src/main/java/com/codingblocks/listviews/CourseRecycleAdapter.java
@@ -24,12 +24,36 @@ public class CourseRecycleAdapter extends
         this.context = context;
     }
 
+
+
     @Override
     public CourseItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater li = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View itemView = li.inflate(R.layout.list_item_course_details, parent, false);
+        int layoutType;
+        switch (viewType) {
+            case 0: layoutType = R.layout.list_item_course_details; break;
+            case 1: layoutType = R.layout.list_item_course_details_center; break;
+            case 2:
+            default: layoutType = R.layout.list_item_course_details_right; break;
+        }
+        View itemView = li.inflate(layoutType, parent, false);
 
         return new CourseItemHolder(itemView);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+
+        Course thisCourse = courses.get(position);
+
+        if (thisCourse.getCourseName().equals("Pandora")) {
+            return 0;
+        }
+        if (thisCourse.getCourseName().equals("Elixir")) {
+            return 1;
+        }
+
+        return 2;
     }
 
     @Override
