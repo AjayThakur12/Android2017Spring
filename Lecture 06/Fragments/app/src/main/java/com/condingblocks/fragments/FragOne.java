123b56914b3ded59e952529c309d45e12385dec8
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-04-08 13:19:41 +0530

    fragment services async network
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

---
+++ Lecture 06/Fragments/app/src/main/java/com/condingblocks/fragments/FragOne.java
@@ -0,0 +1,21 @@
+package com.condingblocks.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by rishabhkhanna on 17/03/17.
+ */
+
+public class FragOne extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        inflater.inflate()
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
