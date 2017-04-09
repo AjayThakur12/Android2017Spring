d2363043910e032b3e30eecc5c9ad9fee25c9ba5
Arnav Gupta <arnav@codingblocks.com>
2017-04-09 12:11:10 +0530

    pause and resume sensor
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture12/HardwareSensors/app/src/main/java/com/codingblocks/hardwaresensors/MainActivity.java
+++ Lecture12/HardwareSensors/app/src/main/java/com/codingblocks/hardwaresensors/MainActivity.java
@@ -1,5 +1,6 @@
 package com.codingblocks.hardwaresensors;
 
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -8,6 +9,7 @@ import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.RelativeLayout;
 
 import java.util.List;
 
@@ -15,16 +17,23 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "SM";
 
+    RelativeLayout relativeLayout;
+    SensorEventListener sel;
+    SensorManager sensMan;
+    Sensor accelSensor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SensorManager sensMan = (SensorManager) getSystemService(SENSOR_SERVICE);
+        relativeLayout = (RelativeLayout) findViewById(R.id.activity_main);
+
+        sensMan = (SensorManager) getSystemService(SENSOR_SERVICE);
 
         List<Sensor> mySensors = sensMan.getSensorList(Sensor.TYPE_ALL);
 
-        Sensor accelSensor = sensMan.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        accelSensor = sensMan.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         for (Sensor sensor : mySensors) {
             Log.d(TAG, "NAME: " + sensor.getName());
@@ -34,13 +43,19 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        SensorEventListener sel = new SensorEventListener() {
+        sel = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
                 Log.d(TAG, "onSensorChanged: ax" + event.values[0]);
                 Log.d(TAG, "onSensorChanged: ay" + event.values[1]);
                 Log.d(TAG, "onSensorChanged: az" + event.values[2]);
                 Log.d(TAG, "onSensorChanged: =======================");
+
+                relativeLayout.setBackgroundColor(Color.rgb(
+                        accel2col(event.values[0]),
+                        accel2col(event.values[1]),
+                        accel2col(event.values[2])
+                ));
             }
 
             @Override
@@ -49,9 +64,27 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
+
+    }
+
+    int accel2col (float accel) {
+        return (int) (((accel + 12) / 24) * 255);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
         sensMan.registerListener(
                 sel,
                 accelSensor,
                 SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    protected void onPause() {
+        sensMan.unregisterListener(sel);
+
+        super.onPause();
     }
 }
