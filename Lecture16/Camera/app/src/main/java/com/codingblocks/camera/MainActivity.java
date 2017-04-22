43eae0990fb268978cf57265626ce226cd0c8baf
Arnav Gupta <arnav@codingblocks.com>
2017-04-22 13:52:02 +0530

    display orientation
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture16/Camera/app/src/main/java/com/codingblocks/camera/MainActivity.java
+++ Lecture16/Camera/app/src/main/java/com/codingblocks/camera/MainActivity.java
@@ -9,8 +9,11 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -45,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
     void checkCameraSizes () {
         camera = Camera.open();
 
-        Camera.Parameters camParams = camera.getParameters();
+        final Camera.Parameters camParams = camera.getParameters();
         for (Camera.Size picSize : camParams.getSupportedPictureSizes()) {
             Log.d(TAG, "picSize: " + picSize.width + " " + picSize.height);
         }
@@ -62,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
             public void surfaceCreated(SurfaceHolder holder) {
                 try {
                     camera.setPreviewDisplay(surfaceHolder);
+                    camera.setDisplayOrientation(90);
                     camera.startPreview();
                 } catch (IOException e) {
                     Log.d(TAG, "surfaceCreated: Could not start preview" );
@@ -71,6 +75,49 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+                WindowManager winMan = (WindowManager) getSystemService(WINDOW_SERVICE);
+                Display display = winMan.getDefaultDisplay();
+
+                if (holder.getSurface() == null) {
+                    return;
+                }
+
+                try {
+                    camera.stopPreview();
+                } catch (Exception e) {
+                    Log.e(TAG, "surfaceChanged: ", e);
+                }
+
+                Camera.Parameters changedParams = camera.getParameters();
+
+                if (display.getRotation() == Surface.ROTATION_0) {
+                    camera.setDisplayOrientation(90);
+
+                }
+                if (display.getRotation() == Surface.ROTATION_90) {
+                    camera.setDisplayOrientation(0);
+
+                }
+                if (display.getRotation() == Surface.ROTATION_180) {
+                    camera.setDisplayOrientation(270);
+
+
+                }
+                if (display.getRotation() == Surface.ROTATION_270) {
+                    camera.setDisplayOrientation(180);
+                }
+
+
+                try {
+                    camera.setParameters(changedParams);
+                    camera.setPreviewDisplay(holder);
+                    camera.startPreview();
+                } catch (Exception e) {
+                    Log.e(TAG, "surfaceChanged: ", e);
+                }
+
+
 
             }
 
@@ -83,11 +130,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onDestroy() {
+    protected void onPause() {
         if (camera != null) {
+            camera.stopPreview();
             camera.release();
         }
-        super.onDestroy();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (camera != null) {
+            try {
+                camera.reconnect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
