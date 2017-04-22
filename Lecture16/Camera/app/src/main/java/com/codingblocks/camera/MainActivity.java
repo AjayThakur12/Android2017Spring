2e80ad8eabd3573087804b3a09783ce35fb29e9d
Arnav Gupta <arnav@codingblocks.com>
2017-04-22 14:17:44 +0530

    take photos
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture16/Camera/app/src/main/java/com/codingblocks/camera/MainActivity.java
+++ Lecture16/Camera/app/src/main/java/com/codingblocks/camera/MainActivity.java
@@ -3,6 +3,8 @@ package com.codingblocks.camera;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.os.Environment;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -13,9 +15,14 @@ import android.view.Display;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     SurfaceView svCamera;
     SurfaceHolder surfaceHolder;
     Camera camera;
+    Button btnTakePhoto;
+    Camera.PictureCallback picCallBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         svCamera = (SurfaceView) findViewById(R.id.surfaceViewCamera);
+        btnTakePhoto = (Button) findViewById(R.id.btnTakePhoto);
 
 
         int camPerm = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
@@ -42,6 +52,49 @@ public class MainActivity extends AppCompatActivity {
         } else {
             checkCameraSizes();
         }
+
+        btnTakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int storePerm = ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                if (storePerm != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            222);
+                } else {
+                    capturePhoto();
+                }
+            }
+        });
+
+        picCallBack = new Camera.PictureCallback() {
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                String fileName = "photo-" + System.currentTimeMillis() + ".jpg";
+                File myPhoto = new File(Environment.getExternalStorageDirectory(), fileName);
+
+                try {
+                    FileOutputStream fos = new FileOutputStream(myPhoto);
+                    fos.write(data);
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    camera.startPreview();
+                } catch (Exception e) {
+                    Log.d(TAG, "onPictureTaken: Could not restart preview", e);
+                }
+            }
+        };
+
+    }
+
+    void capturePhoto() {
+
+        camera.takePicture(null, null, picCallBack);
 
     }
 
@@ -108,7 +161,6 @@ public class MainActivity extends AppCompatActivity {
                     camera.setDisplayOrientation(180);
                 }
 
-
                 try {
                     camera.setParameters(changedParams);
                     camera.setPreviewDisplay(holder);
@@ -133,7 +185,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         if (camera != null) {
             camera.stopPreview();
-            camera.release();
         }
         super.onPause();
     }
@@ -158,6 +209,16 @@ public class MainActivity extends AppCompatActivity {
             if (permissions[0].equals(Manifest.permission.CAMERA)) {
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     checkCameraSizes();
+                }
+            } else {
+                Toast.makeText(this, "You did not give permission", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        if (requestCode == 222) {
+            if (permissions[0].equals(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    capturePhoto();
                 }
             } else {
                 Toast.makeText(this, "You did not give permission", Toast.LENGTH_SHORT).show();
